9630562d165a07735ad8373e9ae4da5333c35d66
JD Martinez <focus4ursoul@gmail.com>
2020-11-25 21:25:22 -0800

    add log in components and key

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login"
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  return <GoogleLogin clientId="762750838273-1vneh1d07qnc37j2kngm03fu5rrsf8nf.apps.googleusercontent.com"/>;
 };
 
 const styles = {
