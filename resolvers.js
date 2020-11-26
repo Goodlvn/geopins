37c63a1d3a5a68a601a7017f6c4de03fdd96b26f
JD Martinez <focus4ursoul@gmail.com>
2020-11-26 09:22:15 -0800

    confirm there is a user context

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,15 @@
-const user = {
-    _id: "1",
-    name: "JD",
-    email: "focus4ursoul@gmail.com",
-    picture: "url"
-}
+const { AuthenticationError } = require("apollo-server")
 
+const authenticated = next => (root, args, ctx, info) => {
+    if (!ctx.currentUser) {
+        throw new AuthenticationError("You must be logged in")
+    }
+
+    return next(root, args, ctx, info);
+} 
 
 module.exports = {
     Query: {
-        me: () => user 
+        me: authenticated((root, args, ctx) => ctx.currentUser)
     }
 }
